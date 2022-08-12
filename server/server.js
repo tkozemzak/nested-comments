422765c17d8c11e4b878371897352cdd12664353
Tim Kozemzak <timkozemzak@gmail.com>
2022-08-12 12:42:07 -0700

    init

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import fastify from "fastify"
+import sensible from "@fastify/sensible"
+import dotenv from "dotenv"
+import { PrismaClient } from '@prisma/client';
+dotenv.config()
+
+const app = fastify()
+app.register(sensible)
+const prisma = new PrismaClient()
+
+app.get("/posts", async (req, res) => {
+    return await commitToDb(prisma.post.findMany({ 
+        select: {
+            id: true,
+            title: true
+        }
+    }))
+})
+
+async function commitToDb(promise) {
+    const [error, data] = await app.to(promise)
+    if (error) return app.httpErrors.internalServerError(error.message)
+    return data;
+}
+
+app.listen({ port: process.env.SERVER_PORT })
